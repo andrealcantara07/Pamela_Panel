10d8ec696bc82572706d68c17f2d3bd3098918f1
aalcantara <aalcantara@alcantara.pc>
2019-01-09 10:13:49 -0500

    removed self placed password column

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
-    validates :first_name, presence: true
-    validates :last_name, presence: true
-    validates :email, presence: true
-    validates :password, presence: true
+    validates :first_name, presence: true, length: {maximum: 30}
+    validates :last_name, presence: true, length: {maximum: 30}
+
+    VALID_EMAIL_REGEX = /\A[w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: {maximum: 255},
+                        format: {with: VALID_EMAIL_REGEX},
+                        uniqueness: {case_sensitive: false}
+    
 end
