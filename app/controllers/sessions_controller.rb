c4d529931dcb1fee429bf21394b8175df735364b
Andre Alcantara <andrealcantara0720@gmail.com>
2019-01-09 21:48:40 -0500

    user side complete

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,23 @@ class SessionsController < ApplicationController
     end
 
     def create
+       user = User.find_by(email: params[:session][:email].downcase)
 
+       if user && user.authenticate(params[:session][:password])
+        session[:user_id] = user.id
+        flash[:success] = "Success"
+        redirect_to user
+       else
+        flash.now[:danger] = ""
+        render 'new'
+       end
+     
     end
 
     def destroy
+        session[:user_id] = nil
+        flash[:success] = "Logged Out"
+        redirect_to root_path
 
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/login', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
   
-  root 'users#index'
+  root 'sessions#new'
 
 
 end
